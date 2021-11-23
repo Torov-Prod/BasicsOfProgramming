7248020d2cece1ca34978c43b15c8452a60e1e10
Torov-Prod <59203946+Torov-Prod@users.noreply.github.com>
2021-11-23 04:54:16 +0300

    Add files via upload

---
+++ task5/programs/program6.py
@@ -0,0 +1,11 @@
+# Определите среднее значение всех элементов последовательности, завершающейся числом 0.
+# -- coding: utf-8 --
+
+sum = 0
+len = 0
+element = int(input())
+while element != 0:
+    sum += element
+    len += 1
+    element = int(input())
+print(sum / len)
